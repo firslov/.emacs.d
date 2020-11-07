84ba46915214acb5976a974990b6386dd4d78fb9
firslov <345989060@qq.com>
2020-11-07 18:50:32 +0800

    202011071850

---
+++ lisp/auto-load.el
@@ -0,0 +1,55 @@
+;;; auto-load.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "auto-indent" "auto-indent.el" (0 0 0 0))
+;;; Generated autoloads from auto-indent.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-indent" '("auto-indent-")))
+
+;;;***
+
+;;;### (autoloads nil "auto-save" "auto-save.el" (0 0 0 0))
+;;; Generated autoloads from auto-save.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "auto-save" '("auto-save-")))
+
+;;;***
+
+;;;### (autoloads nil "init-site" "init-site.el" (0 0 0 0))
+;;; Generated autoloads from init-site.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "init-site" '("auto-save-and-publish-file-mode" "delete-org-and-html" "just-delete-relative-html" "preview-current-buffer-in-browser" "save-and-publish-")))
+
+;;;***
+
+;;;### (autoloads nil "learn-timer" "learn-timer.el" (0 0 0 0))
+;;; Generated autoloads from learn-timer.el
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "learn-timer" '("awesome-tray-module-t" "learn-time" "lt-todo-files" "timer" "todo-num")))
+
+;;;***
+
+;;;### (autoloads nil "phd" "phd.el" (0 0 0 0))
+;;; Generated autoloads from phd.el
+
+(autoload 'mv-articles "phd" "\
+Move articles from downloads to archive." t nil)
+
+(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "phd" '("archive-dir" "if-run-mv" "unread-dir")))
+
+;;;***
+
+;;;### (autoloads nil nil ("phd-autoload.el") (0 0 0 0))
+
+;;;***
+
+(provide 'auto-load)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; auto-load.el ends here
