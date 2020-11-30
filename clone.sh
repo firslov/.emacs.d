d06c4fe2ae5cfe785e198a3131ded5030de552f6
firslov <345989060@qq.com>
2020-11-30 09:11:40 +0800

    202011300912

---
+++ clone.sh
@@ -0,0 +1,6 @@
+git clone --depth=1 https://github.com/manateelazycat/lazycat-theme.git
+git clone --depth=1 https://github.com/manateelazycat/awesome-tab.git
+git clone --depth=1 https://github.com/manateelazycat/awesome-tray.git
+git clone --depth=1 https://github.com/guanyilun/org-equation-live-preview.git
+git clone --depth=1 https://github.com/EvanMeek/shengci.el.git
+git clone --depth=1 https://github.com/reactjs/zh-hans.reactjs.org.git
